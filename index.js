9435f994ad707c0b11e51bc5e1e1781bbc1aa916
Md Nayeem Hossain <nayeemhossain108@gmail.com>
2021-10-29 16:10:22 +0600

    added

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run(){
     try{
         await client.connect();
+        const database=client.db('Services')
+        const productCollection=database.collection('products')
 
       
 
